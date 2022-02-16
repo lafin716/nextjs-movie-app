7a794c3c7a4fee069d57601e1bf14be5cae0a07f
lafinco <lafin716@naver.com>
2022-02-16 23:55:24 +0900

    Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Seo from "../components/Seo";
+
+export default function About() {
+
+    return (
+        <div>
+            <Seo title="About"/>
+            <h2>About</h2>
+        </div>
+    );
+}
---
+++ pages/movies/[id].js
@@ -0,0 +1,11 @@
+
+import { useRouter } from "next/router";
+
+export default function Detail() {
+
+    const router = useRouter();
+
+    return (
+        <h4>{ router.query.title || "Loading..." }</h4>
+    );
+}
